06beadac844ea23227d7850170106aac6126ea05
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-01-17 15:52:27 +0300

    Tab Navigation implemented

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,23 +1,18 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
-import { useRouter } from "expo-router";
-
+import { Text, View } from 'react-native'
+import { Link } from 'expo-router'
+import { useRouter } from 'expo-router'
 
 export default function Index() {
-  const router = useRouter();
+  const router = useRouter()
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Text className="text-red-500 text-2xl font-bold">Hello</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/properties/[id]">Property</Link>
     </View>
-  );
+  )
 }
